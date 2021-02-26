70db1d8152cdc27f28292287202eeb7e6246d154
Daniel Waruo <waruodaniel@gmail.com>
2021-02-26 11:42:34 +0300

    remove validate withdraw form

--- pages/member/wallet/validate-withdraw/[transactionId].js
+++
@@ -1,7 +0,0 @@
-import {withApollo} from "../../../../apollo";
-import ValidateWithdraw from "../../../../components/ValidateWithdraw";
-import {withMemberLayout} from "../../../../components/app";
-
-export default withApollo({ssr: false})(
-  withMemberLayout(ValidateWithdraw, {secure: true})
-);
